ebf99de366ca2c78cb1b7d26e3d909b07f935eca
Krishna Narayan Chaudhary <knc.official.professional@gmail.com>
2020-08-01 17:54:06 +0530

    Global Health Care V 1.0

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function openSlideMenu() {
+    document.getElementById('content-mobile').style.marginRight = '250px';
+    document.getElementById('menu-mobile').style.width = '250px';
+
+}
+function closeSlideMenu() {
+    document.getElementById('content-mobile').style.marginRight = '0';
+    document.getElementById('menu-mobile').style.width = '0';
+
+}
